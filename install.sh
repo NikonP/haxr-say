8d8a2760d8c8d6a6f7df468a4e1cdc6b79189196
NikonP <podgorny.nikon@yandex.ru>
2021-06-13 23:18:06 +0300

    add install.sh and uninstall.sh

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "chmod +x"
+chmod +x haxr-say.sh
+echo "cp to /usr/bin/"
+sudo cp haxr-say.sh /usr/bin/haxr-say
+
+if [ $? -ne 0 ]
+then
+	echo "error!"
+	exit 1
+fi
+
+echo "done!"
+
+if ! command -v lolcat &> /dev/null
+then
+    echo "lolcat is not installed!"
+    echo "diy please :) https://github.com/busyloop/lolcat"
+else
+	echo "lolcat is installed!"
+	echo "yay!"
+fi
---
+++ unistall.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "okay :("
+echo "rm from /usr/bin/"
+sudo rm /usr/bin/haxr-say
+
+if [ $? -ne 0 ]
+then
+	echo "error!"
+	exit 1
+fi
+
+echo "done!"
